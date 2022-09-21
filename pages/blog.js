10e17bf47158d77e45d6c5d4e97c3746c91614b4
mrcoder991 <udaygirhepunje41@gmail.com>
2022-09-21 21:44:01 +0530

    api route to get all data created

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link';
 
 const Blog = () => {
-  return (
-    <div>blog</div>
-  )
-}
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000/api/blogs').then((a) => {
+            return a.json();
+        }).then((parsed) => {
+            setBlogs(parsed)
+        })
+    }, [])
 
-export default Blog
+    return <div className={styles.container}>
+        <main className={styles.main}>
+            {blogs.map((blogitem) => {
+                return <div key={blogitem.slug}>
+                    <Link href={`/blogpost/${blogitem.slug}`}>
+                        <h3 className={styles.blogItemh3}>{blogitem.title}</h3></Link>
+                    <p className={styles.blogItemp}>{blogitem.content.substr(0, 200)}...</p>
+                </div>
+            })}
+        </main>
+    </div>
+};
+
+export default Blog;
